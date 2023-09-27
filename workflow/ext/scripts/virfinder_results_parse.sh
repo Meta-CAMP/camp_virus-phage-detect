14f06ed6be30a3276f7c65e36de5fee3ed63143a
root <root@rtoscan.mcb.bio>
2023-09-27 19:33:41 +0000

    adjusting vibrant database

--- workflow/ext/scripts/virfinder_results_parse.sh
+++ workflow/ext/scripts/virfinder_results_parse.sh
@@ -3,7 +3,11 @@
 # this is a provisory script to just move things forward. 
 # a more elegant solution will be sought!
 
+#cat "$1" | awk -F'\t' '{ if ($4 <= 0.01) print }' | awk -F'_' '{ if ($4 >= 1000) print }' | cut -f2 | sed 's/\"//g' > "$2"
 
-cat  $1 | awk -F'\t' "{ if ( $4 <= 0.01) print }" | awk -F'_' "{ if ( $4 >= 1000) print }" | cut -f2 | sed "s/\"//g" > $2 
+cat $1 | awk -F'\t' '{ if ($4 <= 0.01) print }' |  awk -F'_' '{ if ($4 >= 1000) print }' | cut -f2 | sed 's/\"//g' > $2
+
+
+#cat  $1 | awk -F'\t' "{ if ( $4 <= 0.01) print }" | awk -F'_' "{ if ( $4 >= 1000) print }" | cut -f2 | sed "s/\"//g" > $2 
 
 
